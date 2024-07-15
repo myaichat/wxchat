be0014d1e1c97c609e53c4014c6887056821224c
alex buz <alex_buz@yahoo.com>
2024-07-15 12:03:12 -0400

    init

---
+++ interview.py
@@ -0,0 +1,181 @@
+import os,sys, time
+sys.stdout = open(os.devnull, 'w') #disable pygame prints
+import openai
+from include.fmt import pfmt
+import keyboard
+from pprint import pprint as pp
+from dotenv import load_dotenv
+load_dotenv()
+
+from include.voice import Voice
+from include.transcribe import Transcribe
+from colorama import Fore, Style
+sys.stdout = sys.__stdout__
+# Set your OpenAI API key here
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Initialize the client
+client = openai.OpenAI()
+
+# initialize the voice instance
+model_voice = Voice()
+
+# initialise the voice transcriber
+transcriber = Transcribe()
+
+# Initialize a flag for pausing the stream
+pause_stream = False
+exit_loop = False
+# Define a function to toggle the pause flag when the space key is hit
+def toggle_pause(e):
+    global pause_stream
+    pause_stream = not pause_stream
+def ctrl_set_exit_flag():
+    global exit_loop
+    exit_loop = True
+def set_exit_flag(e):
+    global exit_loop
+    exit_loop = True    
+# Register the space key to call the toggle_pause function
+keyboard.on_press_key('space', toggle_pause)
+keyboard.on_press_key('e', set_exit_flag, suppress=True)
+#keyboard.add_hotkey('ctrl+e', set_exit_flag)
+from colorama import Fore, Style
+
+conversation_history = [
+    {"role": "system", "content": """You are a chatbot that assists with Technical interview for Python developer. 
+     numerate answert options globally with one sequence. Answer in english"""}
+]
+
+def stream_response(prompt):
+    global exit_loop, pause_stream, conversation_history
+    conversation_history.append({"role": "user", "content": prompt})
+    # Create a chat completion request with streaming enabled
+    #pp(conversation_history)
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=conversation_history, 
+
+        stream=True
+    )
+
+    # Print each response chunk as it arrives
+    out=[]
+    inside_stars = False
+    inside_backticks = False
+    inside_hash = False
+    for chunk in response:
+        if exit_loop:
+            print(f"{Style.RESET_ALL}")
+            print("\nExiting loop")
+            
+            break        
+        while pause_stream:
+            time.sleep(0.1) 
+            if exit_loop:
+                print(f"{Style.RESET_ALL}")
+                #pause_stream=False
+                print("\nExiting loop")
+                break                   
+        if hasattr(chunk.choices[0].delta, 'content'):
+            content = chunk.choices[0].delta.content
+            new_content = ''
+            i = 0
+            if not content:
+                continue
+            while i < len(content):
+                if content[i:i+2] == '**':
+                    if inside_stars:
+                        new_content += f"{Style.RESET_ALL}"
+                        inside_stars = False
+                    else:
+                        new_content += f"{Fore.GREEN}{Style.BRIGHT}"
+                        inside_stars = True
+                    i += 2  # Skip the next character
+                elif content[i:i+3] == '```':
+                    if inside_backticks:
+                        new_content += f"{Style.RESET_ALL}"
+                        inside_backticks = False
+                        i += 3 
+                    else:
+                        new_content += f"{Fore.RED}{Style.BRIGHT}"
+                        inside_backticks = True
+                        i += 3
+                     # Skip the next two characters
+                elif content[i] == '#' and (i == 0 or content[i-1] == '\n'):  # If the line starts with '#'
+                    new_content += f"{Fore.BLUE}{Style.BRIGHT}" + content[i]
+                    inside_hash = True
+                    i += 1
+                elif content[i] == '\n' and inside_hash:  # If the line ends and we're inside a hash line
+                    new_content += f"{Style.RESET_ALL}" + content[i]
+                    inside_hash = False
+                    i += 1
+                else:
+                    new_content += content[i]
+                    i += 1
+            print(new_content, end='', flush=True)
+            if new_content:
+                out.append(new_content)
+    
+    if inside_backticks:  # If we're still inside a code block, add the reset code
+        out.append(Style.RESET_ALL)
+    conversation_history.append({"role": "assistant", "content": ''.join(out)})
+    return ''.join(out), exit_loop
+
+def listen():
+    
+    file_path = transcriber.record_audio()
+    print(f"Audio recorded and saved to {file_path}")
+
+    # Transcribe audio
+    transcript_result = transcriber.transcribe_audio(file_path)
+    #print(f"Transcription Result: {transcript_result}")
+    return transcript_result
+
+def conversation():
+    global exit_loop, pause_stream
+    #prompt="Hey,  what is the fastest way to sort in python?"
+    user_input = ""
+    
+    while True:
+        print('listening...')
+        user_input = listen()
+        #print('User input:', user_input)
+        if user_input is None:
+            print('No input')
+            user_input = listen()
+            #print('User input 2:', user_input)
+
+        elif "bye" in user_input.lower():
+            #respond(conversation_chain.run({"question": "Send a friendly goodbye question and give a nice short sweet compliment based on the conversation."}))
+            model_voice.delete_mp3_files()  # delete all the model response audio files
+            transcriber.delete_wav_audio_files()    # delete all the user recorded speech
+            return
+        
+        else:
+            assert user_input
+            
+            pfmt([[ user_input]], ['User Input'])          
+            model_response, tell_me_more =stream_response(user_input)
+            if not tell_me_more:
+                print()
+                inp = input('Continue?')
+            else:
+                exit_loop=False
+                pause_stream=False
+            #print('Memory:')
+            #print(memory)
+            
+            #print(prompt)
+            #respond(model_response)
+
+
+
+if __name__ == "__main__":
+
+    conversation()
+    #stream_response("Hey,  what is the fastes way to sort in python?")
+    
+
+
+
